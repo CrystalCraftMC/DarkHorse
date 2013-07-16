5f49fa15ad3909bcb478a00d543aaac56c538dc4
jflory7 <jflory7@gmail.com>
2013-07-16 19:00:34 -0400

    First commit

---
+++ src/com/crystalcraftmc/darkhorse/Main.java
@@ -0,0 +1,26 @@
+package com.crystalcraftmc.darkhorse;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin
+{
+	@Override
+    public void onEnable()
+	{
+        // TODO Insert logic to be performed when the plugin is enabled
+		getLogger().info("PlayerLink has been enabled!");
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("skeletonhorse").setExecutor(new Skeleton(this));
+		
+		// This will throw a NullPointerException if you don't have the command defined in your plugin.yml file!
+		getCommand("zombiehorse").setExecutor(new Zombie(this));
+	}
+	
+    @Override
+    public void onDisable()
+    {
+        // TODO Insert logic to be performed when the plugin is disabled
+    	getLogger().info("PlayerLink has been disabled!");
+    }
+}
---
+++ src/com/crystalcraftmc/darkhorse/Skeleton.java
@@ -0,0 +1,40 @@
+package com.crystalcraftmc.darkhorse;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class Skeleton implements CommandExecutor
+{
+	Main plugin;
+	public Skeleton(Main plugin)
+	{
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+	{
+		// Make the letter 'p' a variable for the command sender (or the player).
+		Player p = (Player) sender;
+		
+		// If the player typed /twitter, then do the following...
+    	if (cmd.getName().equalsIgnoreCase("skeletonhorse"))
+    	{
+    		Location location = p.getLocation();
+    		p.getWorld().spawnEntity(location, EntityType.HORSE);
+    		p.sendMessage(ChatColor.GOLD + "Skeleton horse has been spawned.");
+    		Bukkit.broadcastMessage(ChatColor.GOLD + "A new mob has appeared at " + location);
+    		
+    		// If this has happened, the function will return true. 
+    		return true;
+    	}
+    	// If this hasn't happened, a value of false will be returned.
+    	return false;
+	}
+}
