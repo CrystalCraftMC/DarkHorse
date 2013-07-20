22e42c02d0a427b174b5cfee1539ad1fe59e5181
Justin Flory <jflory7@gmail.com>
2013-07-20 04:37:28 -0400

    Revert "Tried changing variants"
    This reverts commit 0f05b637245ae50488fe323d1c076e0da1a49ae4.

--- src/com/crystalcraftmc/darkhorse/Horse.java
+++ src/com/crystalcraftmc/darkhorse/Horse.java
@@ -17,22 +17,7 @@ public class Horse implements CommandExecutor
 		this.plugin = plugin;
 	}
 	
-	private void setVariant1(Variant horse)
-	{
-		// TODO Auto-generated method stub
-	}
-	
-	private void setVariant2(Variant donkey)
-	{
-		// TODO Auto-generated method stub
-	}
-	
-	private void setVariant3(Variant skeleton_horse)
-	{
-		// TODO Auto-generated method stub
-	}
-	
-	private void setVariant4(Variant undead_horse)
+	private void setVariant(Variant horse)
 	{
 		// TODO Auto-generated method stub
 	}
@@ -45,11 +30,6 @@ public class Horse implements CommandExecutor
 	    	// Make the letter 'p' a variable for the command sender (or the player).
 			Player p = (Player) sender;
 			
-    		// Make 'location' a variable for the player's location.
-    		Location location = p.getLocation();
-			
-    		Horse horse = (Horse) location.getWorld().spawnEntity(location, EntityType.HORSE);
-			
 	        if(args.length != 1)
 	        {
 	            return false;
@@ -60,8 +40,14 @@ public class Horse implements CommandExecutor
 	        	// If the sender of the command has this permission...
 	    		if(p.hasPermission("darkhorse.normal"))
 	    		{
+	        		// ...create a variable to find the player's location...
+	        		Location location = p.getLocation();
+	        		
+	        		// ...then spawn a horse at the player's current location...
+	        		Horse horse = (Horse) location.getWorld().spawnEntity(location, EntityType.HORSE);
+	        		
 	        		// ...and change the type of horse to a normal horse.
-	        		horse.setVariant1(Variant.HORSE);
+	        		horse.setVariant(Variant.HORSE);
 	        		
 	        		// Then, notify the player that the entity has been spawned.
 	        		p.sendMessage(ChatColor.GOLD + "A normal horse has been spawned.");
@@ -76,8 +62,14 @@ public class Horse implements CommandExecutor
 	        	// If the sender of the command has this permission...
 	    		if(p.hasPermission("darkhorse.donkey"))
 	    		{
-	    			// ...and change the type of horse to a donkey.
-	        		horse.setVariant2(Variant.DONKEY);
+	        		// ...create a variable to find the player's location...
+	        		Location location = p.getLocation();
+	        		
+	        		// ...then spawn a horse at the player's current location...
+	        		Horse horse = (Horse) location.getWorld().spawnEntity(location, EntityType.HORSE);
+	        		
+	        		// ...and change the type of horse to a donkey.
+	        		horse.setVariant(Variant.DONKEY);
 	        		
 	        		// Then, notify the player that the entity has been spawned.
 	        		p.sendMessage(ChatColor.GOLD + "A donkey has been spawned.");
@@ -92,8 +84,14 @@ public class Horse implements CommandExecutor
 	        	// If the sender of the command has this permission...
 	    		if(p.hasPermission("darkhorse.skeleton"))
 	    		{
+	        		// ...create a variable to find the player's location...
+	        		Location location = p.getLocation();
+	        		
+	        		// ...then spawn a horse at the player's current location...
+	        		Horse horse = (Horse) location.getWorld().spawnEntity(location, EntityType.HORSE);
+	        		
 	        		// ...and change the type of horse to a skeleton horse.
-	        		horse.setVariant3(Variant.SKELETON_HORSE);
+	        		horse.setVariant(Variant.SKELETON_HORSE);
 	        		
 	        		// Then, notify the player that the entity has been spawned.
 	        		p.sendMessage(ChatColor.GOLD + "A skeleton horse has been spawned.");
@@ -107,9 +105,15 @@ public class Horse implements CommandExecutor
 	        {
 	        	// If the sender of the command has this permission...
 	    		if(p.hasPermission("darkhorse.zombie"))
-	    		{        		
+	    		{
+	        		// ...create a variable to find the player's location...
+	        		Location location = p.getLocation();
+	        		
+	        		// ...then spawn a horse at the player's current location...
+	        		Horse horse = (Horse) location.getWorld().spawnEntity(location, EntityType.HORSE);
+	        		
 	        		// ...and change the type of horse to a zombie horse.
-	        		horse.setVariant4(Variant.UNDEAD_HORSE);
+	        		horse.setVariant(Variant.UNDEAD_HORSE);
 	        		
 	        		// Then, notify the player that the entity has been spawned.
 	        		p.sendMessage(ChatColor.GOLD + "A zombie horse has been spawned.");
