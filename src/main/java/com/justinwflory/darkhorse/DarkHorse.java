4e684b9a4703df56c6288b1beeca18cd6c84a25a
Justin W. Flory <jflory7.v2@gmail.com>
2014-10-03 10:58:29 -0400

    Syntax: Move private method to bottom of class

--- src/main/java/com/justinwflory/darkhorse/DarkHorse.java
+++ src/main/java/com/justinwflory/darkhorse/DarkHorse.java
@@ -1,5 +1,7 @@
 /*
- * Copyright (c) 2014, Justin W. Flory
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Justin W. Flory
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -7,17 +9,16 @@
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package com.justinwflory.darkhorse;
@@ -61,19 +62,6 @@ public class DarkHorse extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("DarkHorse v1.2.2 has been disabled!");
-    }
-
-    private void spawnHorse(Player player, Variant variant, boolean tamed, boolean chest) {
-        Location location = player.getLocation();
-        Horse horse = location.getWorld().spawn(location, Horse.class);
-        horse.setVariant(variant);
-        if (tamed) {
-            horse.setTamed(true);
-            horse.setOwner(player);
-        }
-        if (chest) {
-            horse.setCarryingChest(true);
-        }
     }
 
     private void notify(Player player) {
@@ -196,5 +184,18 @@ public class DarkHorse extends JavaPlugin {
             }
         }
         return false;
+    }
+
+    private void spawnHorse(Player player, Variant variant, boolean tamed, boolean chest) {
+        Location location = player.getLocation();
+        Horse horse = location.getWorld().spawn(location, Horse.class);
+        horse.setVariant(variant);
+        if (tamed) {
+            horse.setTamed(true);
+            horse.setOwner(player);
+        }
+        if (chest) {
+            horse.setCarryingChest(true);
+        }
     }
 }
